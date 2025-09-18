5a491d13470a6bced94b802026a6fee9fcd39f93
abhisirohi72 <abhisirohi72@gmail.com>
2025-09-18 18:23:39 +0530

    M:modify file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,30 +8,46 @@
     <title>Login - ChargingBD</title>
     <link href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    @if((isset($page_name)) && $page_name=="login")
-        <link rel="stylesheet" href="{{ asset('public/assets/css/login.css') }}">
-    @elseif((isset($page_name)) && $page_name=="forgot_user")
-        <link rel="stylesheet" href="{{ asset('public/assets/css/forgot_pass.css') }}">
-    @elseif((isset($page_name)) && $page_name=="register_user")
-        <link rel="stylesheet" href="{{ asset('public/assets/css/register.css') }}">
+    @if (isset($page_name) && $page_name == 'login')
+        @if (env('APP_URL') == 'http://localhost/new_project')
+            <link rel="stylesheet" href="{{ asset('assets/css/login.css') }}">
+        @else
+            <link rel="stylesheet" href="{{ asset('public/assets/css/login.css') }}">
+        @endif
+    @elseif(isset($page_name) && $page_name == 'forgot_user')
+        @if (env('APP_URL') == 'http://localhost/new_project')
+            <link rel="stylesheet" href="{{ asset('assets/css/forgot_pass.css') }}">
+        @else
+            <link rel="stylesheet" href="{{ asset('public/assets/css/forgot_pass.css') }}">
+        @endif
+    @elseif(isset($page_name) && $page_name == 'register_user')
+        @if (env('APP_URL') == 'http://localhost/new_project')
+            <link rel="stylesheet" href="{{ asset('assets/css/register.css') }}">
+        @else
+            <link rel="stylesheet" href="{{ asset('public/assets/css/register.css') }}">
+        @endif
     @else
-        <link rel="stylesheet" href="{{ asset('public/assets/css/u_dashboard.css') }}">
+        @if (env('APP_URL') == 'http://localhost/new_project')
+            <link rel="stylesheet" href="{{ asset('assets/css/u_dashboard.css') }}">
+        @else
+            <link rel="stylesheet" href="{{ asset('public/assets/css/u_dashboard.css') }}">
+        @endif
     @endif
 </head>
 
 <body>
-    @if(session()->has('user'))
-        @include("layouts.user.header")
-        @include("layouts.user.sidebar")
-    @elseif(session()->has('admin')) 
-        @include("layouts.admin.header")
-        @include("layouts.admin.sidebar")
-    @elseif(session()->has('developer')) 
-        @include("layouts.developer.header")
-        @include("layouts.developer.sidebar")    
+    @if (session()->has('user'))
+        @include('layouts.user.header')
+        @include('layouts.user.sidebar')
+    @elseif(session()->has('admin'))
+        @include('layouts.admin.header')
+        @include('layouts.admin.sidebar')
+    @elseif(session()->has('developer'))
+        @include('layouts.developer.header')
+        @include('layouts.developer.sidebar')
     @endif
     @yield('content') <!-- Main Content -->
-    @if(($page_name!="login") || ($page_name!="forgot_user") || ($page_name!="register_user"))
+    @if ($page_name != 'login' && $page_name != 'forgot_user' && $page_name != 'register_user')
         @include('layouts.footer')
     @endif
     @stack('script-push')
